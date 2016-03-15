cd6a0b69442770160be51dc7a211b7040ad17857
aluh <adriano.carapeto@yahoo.com.br>
2016-03-15 02:25:09 -0300

    Update Config_Master.hpp

--- World.Altis/Config_Master.hpp
+++ World.Altis/Config_Master.hpp
@@ -37,8 +37,8 @@ class Life_Settings {
     house_limit = 5; //Maximum amount of houses a player can own.
 
     /* Gang related settings */
-    gang_price = 75000; //Price for creating a gang, remember they are persistent so keep it reasonable to avoid millions of gangs.
-    gang_upgradeBase = 10000; //The base cost for upgrading slots in a gang
+    gang_price = 350000; //Price for creating a gang, remember they are persistent so keep it reasonable to avoid millions of gangs.
+    gang_upgradeBase = 100000; //The base cost for upgrading slots in a gang
     gang_upgradeMultiplier = 2.5; //Not sure if in use?
 
     /* Player-related systems */
